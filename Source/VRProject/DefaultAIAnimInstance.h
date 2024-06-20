90abfa7e37bd660eed22d9d4eee9062839cb3a1e
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-06-20 16:13:59 +0900

    DefaultAIAnimInstance

---
+++ Source/VRProject/DefaultAIAnimInstance.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstance.h"
+#include "DefaultAIAnimInstance.generated.h"
+
+/**
+ * 
+ */
+
+UENUM(BlueprintType)
+enum class EAIAnimType : uint8
+{
+	Idle,
+	Walk,
+	Run,
+	Attack,
+	Death,
+	Skill1,
+	Skill2,
+	Skill3,
+	Interaction1
+};
+
+
+UCLASS()
+class VRPROJECT_API UDefaultAIAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UDefaultAIAnimInstance();
+
+protected:
+	UPROPERTY(Category = Anim, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	EAIAnimType	mAnimType;
+
+public:
+	void ChangeAnim(EAIAnimType Type)
+	{
+		mAnimType = Type;
+	}
+
+public:
+	virtual void NativeInitializeAnimation();
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds);
+
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds);
+
+	virtual void NativePostEvaluateAnimation();
+
+	virtual void NativeUninitializeAnimation();
+
+	virtual void NativeBeginPlay();	
+};
